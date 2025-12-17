38e2000a0b35ee3d26f6ae80bc83487e25aa6f3a
icweaver <weaveric@gmail.com>
2025-12-17 04:14:24 +0000

    Deploying to gh-pages from @ icweaver/onaketa-org@3114941c6df25f5e98b263651eb79c25a528f4fe 🚀

---
+++ students/isaiah_alexander/boards.jl
@@ -0,0 +1,508 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 1dedaa68-4f52-471a-ab41-c04ebbd793ca
+begin
+	using PlutoUI
+	using HypertextLiteral: @htl
+end
+
+# ╔═╡ d0bd18f5-f50f-47cf-b39c-ec3ee9183304
+md"""
+Below are the boards from our sessions. Hover over the table of contents icon to show/hide it. Click and drag to move around, and hold control while scrolling to zoom. Tap-and-drag, and pinch-and-zoom if on mobile 🚀
+"""
+
+# ╔═╡ 45d0f413-6543-48b6-a269-3292c27261a6
+board(date, description, url_share) = """
+## ▪ $(date)
+
+$(description)
+
+👉🏾 [Link to board]($(url_share))
+""" |> Markdown.parse;
+
+# ╔═╡ d8843210-9c3c-43c3-bcb2-70171899d9d1
+board(
+	"2025-10-06",
+	md"""###### `fractions` `LCD`
+	!!! note "Summary"
+		Reviewed homework on re-writing fractions using their least common denominator (LCD). We then used this to start comparing different fractions with each other.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/Acf1ztSlzVp",
+)
+
+# ╔═╡ 288f5b88-ce22-4f5d-9323-67eac543cd86
+board(
+	"2025-10-13",
+	md"""###### `mixed fractions` `improper fractions`
+	!!! note "Summary"
+		Reviewed working with mixed and improper fractions, and how to convert between them.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/7qoA1zdsfmx",
+)
+
+# ╔═╡ 24f589dc-6805-4eb8-8c78-8f6ca88f1592
+board(
+	"2025-10-20",
+	md"""###### `mixed fractions` `improper fractions`
+	!!! note "Summary"
+		* Reviewed [1.1 - 1.3 Practice Test](https://docs.google.com/document/d/1Vhi75VGQdi8ZNJsARfC_-S5Lj9NraTXPDSAvZepR5dM/edit?usp=sharing) and [Ch1.6e-2.3 Practice Test](https://docs.google.com/document/d/1zQ4IXt9J_XnsnxidV9QxAQ8g8BX39L0nxqTmGS8JWJ8/edit?usp=sharing) for test tomorrow. 
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/5fVYFJqCwr",
+)
+
+# ╔═╡ bbf985d2-3b66-4e1a-8ff0-e4f7fa50d8e9
+board(
+	"2025-10-27 🎃",
+	md"""###### `mixed fractions` `improper fractions`
+	!!! note "Summary"
+		* Reviewed [Math 6: Chapter 2 Fractions Practice Test](https://docs.google.com/document/d/1zQ4IXt9J_XnsnxidV9QxAQ8g8BX39L0nxqTmGS8JWJ8/edit?usp=sharing) for test re-take tomorrow. 
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/3gq2g4Twoza",
+)
+
+# ╔═╡ ffc3dfcf-c234-42c5-b919-b98f531bc226
+board(
+	"2025-11-03",
+	md"""###### `multiplication` `decimals`
+	!!! note "Summary"
+		* Reviewed multiplying decimals with multiple digits.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/8V8pANsBj9l",
+)
+
+# ╔═╡ d41b6505-73aa-4926-8802-073017885eab
+board(
+	"2025-11-10",
+	md"""###### `multiplication` `decimals`
+	!!! note "Summary"
+		* Continued review of multiplying decimals with multiple digits.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/4AltSIKagnd",
+)
+
+# ╔═╡ d5ba8b72-2860-4bbe-8f55-d874c629334d
+board(
+	"2025-11-17",
+	md"""###### `ratios` `proportinos`
+	!!! note "Summary"
+		Reviewed creating and simplifying proportions from word problems.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/9xkVqwlBVOJ",
+)
+
+# ╔═╡ 483de8d0-2940-4a1e-aecb-76c58e444b3d
+board(
+	"2025-12-01",
+	md"""###### `ratio tables` `algebra`
+	!!! note "Summary"
+		No new material from class. Instead, we reviewed working with ratio tables, and then got a headstart on new content: solving linear equations.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/5ViaH5Y2OoH",
+)
+
+# ╔═╡ c885bfe0-996c-49ad-9800-6b4e80d2b4d0
+board(
+	"2025-12-08",
+	md"""###### `unit rates` `equivalent rates`
+	!!! note "Summary"
+		Reviewed using unit and equivalent rates to solve word problems.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/4Bxqu1C6OGU",
+)
+
+# ╔═╡ 1154cf55-d77e-4a04-b22f-2d2f25e8705d
+board(
+	"2025-12-15",
+	md"""###### `unit rates` `equivalent rates`
+	!!! note "Summary"
+		Final review on unit and equivalent rates to solve word problems.
+	""",
+	"https://link.excalidraw.com/l/570Ui7jcPf6/ApzO8Qp9BhX",
+)
+
+# ╔═╡ cc17e1bf-acbf-4e95-b28b-3b77e5ae4e69
+iframe(url) = @htl """
+<iframe src="$(url)" width="100%" height=800 style="border: none;"></iframe>
+""";
+
+# ╔═╡ 8783183d-4fe0-413c-bab5-3aa7ac497036
+iframe("https://link.excalidraw.com/readonly/8xGkg6liMPmXsLtvCZDZ?darkMode=true")
+
+# ╔═╡ c3116396-50d8-479d-a88a-aaff6925d0ad
+iframe("https://link.excalidraw.com/readonly/dn12J9qErW34GKZNmBoJ?darkMode=true")
+
+# ╔═╡ e1685f29-94c4-48c8-b6cc-74dea0624198
+iframe("https://link.excalidraw.com/readonly/5LetYLGAmbQ4u0T5S186?darkMode=true")
+
+# ╔═╡ 11638468-359c-4fb0-9d89-28ce72625b2a
+iframe("https://link.excalidraw.com/readonly/DS6YKp9ejT4RqagsBrru?darkMode=true")
+
+# ╔═╡ 25b46726-142c-4323-91b9-347514176bad
+iframe("https://link.excalidraw.com/readonly/6dZ6QvLJyQtgFVanM9WT?darkMode=true")
+
+# ╔═╡ d6bbe93f-49e2-456e-afbd-f0bf633faddf
+iframe("https://link.excalidraw.com/readonly/XvVB1zobmNrdsMO3Xke5?darkMode=true")
+
+# ╔═╡ fd9455d5-9727-4e63-bae0-49202d33caec
+iframe("https://link.excalidraw.com/readonly/h2fBHyyBuEMpH42XDTZy?darkMode=true")
+
+# ╔═╡ 7e365ecb-bc48-4bae-a8eb-119de13cdc05
+iframe("https://link.excalidraw.com/readonly/ZKT747u1qOKYenyNUSLl?darkMode=true")
+
+# ╔═╡ 70e90a5e-037c-41e4-9893-1f0c6d5f929b
+iframe("https://link.excalidraw.com/readonly/0xD4OwRMp4saJBSYYqpT?darkMode=true")
+
+# ╔═╡ fa0e4214-ba6d-4d4f-bbe8-bd1c4a2fbaff
+iframe("https://link.excalidraw.com/readonly/eCJaQlG1tflG9dIRdAyf?darkMode=true")
+
+# ╔═╡ f2c740f5-c472-43e5-93a1-10f8a8964e5d
+TableOfContents(; title="Isaiah's board 🏀", depth=6, indent=false)
+
+# ╔═╡ cabd7532-3705-4caf-82a5-12f9fd5a47f2
+html"""
+<style>
+main {
+    max-width: 90%;
+    margin-right: 5% !important;
+}
+
+pluto-output.rich_output code {
+  background-color: light-dark(wheat, grey);
+}
+</style>
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+HypertextLiteral = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+HypertextLiteral = "~0.9.5"
+PlutoUI = "~0.7.59"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.1"
+manifest_format = "2.0"
+project_hash = "026533f4d5798b427cf78f9e543c536dc326e6b6"
+
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "6e1d2a35f2f90a4bc7c2ed98079b2ba09c35b83a"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.3.2"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.2"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+version = "1.11.0"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+version = "1.11.0"
+
+[[deps.ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "b10d0b65641d57b8b4d5e234446582de5047050d"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.5"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.3.0+1"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+version = "1.11.0"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+version = "1.11.0"
+
+[[deps.FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "05882d6995ae5c12bb5f36dd2ed3f61c98cbb172"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.5"
+
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "179267cfa5e712760cd43dcae385d7ea90cc25a4"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.5"
+
+[[deps.HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.5"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "b6d6bfdd7ce25b0f9b2f6b3dd56b2673a66c8770"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.5"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+version = "1.11.0"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.4"
+
+[[deps.JuliaSyntaxHighlighting]]
+deps = ["StyledStrings"]
+uuid = "ac6e5ff7-fb65-4e79-a425-ec3bc9c03011"
+version = "1.12.0"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.4"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "OpenSSL_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "8.11.1+1"
+
+[[deps.LibGit2]]
+deps = ["LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+version = "1.11.0"
+
+[[deps.LibGit2_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "OpenSSL_jll"]
+uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
+version = "1.9.0+0"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "OpenSSL_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.11.3+1"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+version = "1.11.0"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+version = "1.12.0"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+version = "1.11.0"
+
+[[deps.MIMEs]]
+git-tree-sha1 = "1833212fd6f580c20d4291da9c1b4e8a655b128e"
+uuid = "6c6e2e6c-3030-632d-7369-2d6c69616d65"
+version = "1.0.0"
+
+[[deps.Markdown]]
+deps = ["Base64", "JuliaSyntaxHighlighting", "StyledStrings"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+version = "1.11.0"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+version = "1.11.0"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2025.5.20"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.3.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.29+0"
+
+[[deps.OpenSSL_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "458c3c95-2e84-50aa-8efc-19380b2a3a95"
+version = "3.5.1+0"
+
+[[deps.Parsers]]
+deps = ["Dates", "PrecompileTools", "UUIDs"]
+git-tree-sha1 = "8489905bcdbcfac64d1daa51ca07c0d8f0283821"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.8.1"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "Random", "SHA", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.12.0"
+
+    [deps.Pkg.extensions]
+    REPLExt = "REPL"
+
+    [deps.Pkg.weakdeps]
+    REPL = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
+git-tree-sha1 = "7e71a55b87222942f0f9337be62e26b1f103d3e4"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.61"
+
+[[deps.PrecompileTools]]
+deps = ["Preferences"]
+git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
+uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
+version = "1.2.1"
+
+[[deps.Preferences]]
+deps = ["TOML"]
+git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
+uuid = "21216c6a-2e73-6563-6e65-726566657250"
+version = "1.4.3"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+version = "1.11.0"
+
+[[deps.Random]]
+deps = ["SHA"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+version = "1.11.0"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+version = "1.11.0"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra"]
+git-tree-sha1 = "ae3bb1eb3bba077cd276bc5cfc337cc65c3075c0"
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+version = "1.11.1"
+
+    [deps.Statistics.extensions]
+    SparseArraysExt = ["SparseArrays"]
+
+    [deps.Statistics.weakdeps]
+    SparseArrays = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+
+[[deps.StyledStrings]]
+uuid = "f489334b-da3d-4c2e-b8f0-e476e12c162b"
+version = "1.11.0"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.3"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+version = "1.11.0"
+
+[[deps.Tricks]]
+git-tree-sha1 = "6cae795a5a9313bbb4f60683f7263318fc7d1505"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.10"
+
+[[deps.URIs]]
+git-tree-sha1 = "67db6cc7b3821e19ebe75791a9dd19c9b1188f2b"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.5.1"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+version = "1.11.0"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+version = "1.11.0"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.3.1+2"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.15.0+0"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.64.0+1"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.5.0+2"
+"""
+
+# ╔═╡ Cell order:
+# ╟─d0bd18f5-f50f-47cf-b39c-ec3ee9183304
+# ╟─d8843210-9c3c-43c3-bcb2-70171899d9d1
+# ╟─8783183d-4fe0-413c-bab5-3aa7ac497036
+# ╟─288f5b88-ce22-4f5d-9323-67eac543cd86
+# ╟─c3116396-50d8-479d-a88a-aaff6925d0ad
+# ╟─24f589dc-6805-4eb8-8c78-8f6ca88f1592
+# ╟─e1685f29-94c4-48c8-b6cc-74dea0624198
+# ╟─bbf985d2-3b66-4e1a-8ff0-e4f7fa50d8e9
+# ╟─11638468-359c-4fb0-9d89-28ce72625b2a
+# ╟─ffc3dfcf-c234-42c5-b919-b98f531bc226
+# ╟─25b46726-142c-4323-91b9-347514176bad
+# ╟─d41b6505-73aa-4926-8802-073017885eab
+# ╟─d6bbe93f-49e2-456e-afbd-f0bf633faddf
+# ╟─d5ba8b72-2860-4bbe-8f55-d874c629334d
+# ╟─fd9455d5-9727-4e63-bae0-49202d33caec
+# ╟─483de8d0-2940-4a1e-aecb-76c58e444b3d
+# ╟─7e365ecb-bc48-4bae-a8eb-119de13cdc05
+# ╟─c885bfe0-996c-49ad-9800-6b4e80d2b4d0
+# ╟─70e90a5e-037c-41e4-9893-1f0c6d5f929b
+# ╟─1154cf55-d77e-4a04-b22f-2d2f25e8705d
+# ╟─fa0e4214-ba6d-4d4f-bbe8-bd1c4a2fbaff
+# ╟─45d0f413-6543-48b6-a269-3292c27261a6
+# ╟─cc17e1bf-acbf-4e95-b28b-3b77e5ae4e69
+# ╟─f2c740f5-c472-43e5-93a1-10f8a8964e5d
+# ╟─cabd7532-3705-4caf-82a5-12f9fd5a47f2
+# ╟─1dedaa68-4f52-471a-ab41-c04ebbd793ca
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
